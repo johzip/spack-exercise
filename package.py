f0c3643896b38035dc3be30e329dcae78b9bac47
johzip <56304247+johzip@users.noreply.github.com>
2025-01-12 12:34:58 +0100

    Update package.py
    at least version 0.7.0
    
    Co-authored-by: Ishaan Desai <ishaan.desai@ipvs.uni-stuttgart.de>

--- package.py
+++ package.py
@@ -24,7 +24,7 @@ class SpackExercise(CMakePackage):
 
     depends_on("cxx", type="build")
 
-    depends_on("yaml-cpp@0.7.0", when="@0.3.0")
+    depends_on("yaml-cpp@0.7.0:", when="@0.3.0")
     depends_on("boost@1.65.1:", when="@0.2.0:0.3.0")
  
 
